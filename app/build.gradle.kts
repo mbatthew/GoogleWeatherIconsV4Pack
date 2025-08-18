2d98130ac80279cfd5970da31880e57424030d22
mbatthew <mbatthewt@gmail.com>
2025-08-18 05:58:28 -0600

    Add dark mode screenshot to README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.matthew.googleweathericonsv4"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
